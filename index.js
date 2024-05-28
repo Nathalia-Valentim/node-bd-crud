4b2fe14abc33793777753d3bfc942490ff966bb3
MASTER <MASTER@DSN1191109156.edu119.local>
2024-05-28 11:03:36 -0300

    banco

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 const PORT = 3000;
 const connectionString = "server=DSN1191109156\SQLEXPRESS;Database=CARROS;Trusted_Connection=Yes;Driver={Sql Server Native Cliente 11.0}";
 
-//Leitura
+//leitura
 app.get("/carros", (req, res) => {
     sql.query(connectionString, "SELECT * FROM carros", (erro, rows) => {
         if (erro) {
@@ -16,7 +16,23 @@ app.get("/carros", (req, res) => {
         } else {
             res.status(200).json(rows);
         }
-    })
-})
+    });
+});
+
+//escrita 
+app.post("/carros", (req, res) => {
+    const { modelo, marca } = req.body;
+    sql.query(
+        connectionString,
+        `INSERT INTO carros VALUES ('${ modelo }', '${ marca }')`,
+        (erro, rows) => {
+            if(erro) {
+                res.status(500).json("Erro Interno de Servidor");
+            } else {
+                res.status(201).json("Cadastrado com sucesso!");
+            }
+        }
+    );
+});
 
 app.listen(PORT, () => console.log(`Server rodando na porta ${PORT}`));
